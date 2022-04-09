c20a60b40c2548e3cbd6a180806fdba75eb775bf
ukly <dlwjddnr5438@kookmin.ac.kr>
2022-04-09 04:10:44 +0000

    first commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,7 +12,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -20,3 +24,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
